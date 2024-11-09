225ff14012b3a54522a0008ce243b6dad3d6c697
John <jmcohen64@gmail.com>
2024-11-09 12:26:35 -0700

    friday fixes;

--- companion.m
+++ companion.m
@@ -1,13 +1,14 @@
 function A = companion(v)
     % returns companion matrix for any matrix A
     n = length(v);
-    A = sparse(n,n);
+    dim = n-1;
+    A = sparse(dim,dim);
     for i=1:n
         for j=1:n
             if (j-i == 1)
                 A(i,j) = 1;
             elseif (i == n)
-                A(i,j) = -1*v(j);
+                A(i,j) = -1*v(dim-(j-1));
             end
         end
     end
--- power_method2d.m
+++ power_method2d.m
@@ -6,6 +6,7 @@ function [rp_n, rm_n] = power_method2d(A, x1, x2, tol, max_iter)
     n = length(A);
     if isempty(x1)
         x1 = (randi(10,1,n)).';
+        %disp(x1);
     end
     if isempty(x2)
         x2 = (randi(10,1,n)).';
@@ -13,13 +14,14 @@ function [rp_n, rm_n] = power_method2d(A, x1, x2, tol, max_iter)
 
     rp_n = 0;
     rm_n = 0;
-
+    
+    iter  = 1;
     for i=1:max_iter
         y1 = A*x1;
         y2 = A*x2;
         q1 = y1/norm(y1,2);
         q2 = y2-((q1.')*y2)*q1;
-        q2 = y2/norm(q2,2);
+        q2 = q2/norm(q2,2);
         B11 = (q1.')*(A*q1);
         B12 = (q1.')*(A*q2);
         B21 = (q2.')*(A*q1);
@@ -28,13 +30,19 @@ function [rp_n, rm_n] = power_method2d(A, x1, x2, tol, max_iter)
         del = ((B11-B22)/2)^2+B12*B21;
         rp_n1 = trace + sqrt(del);
         rm_n1 = trace - sqrt(del);
-        if (norm(A*q2-rm_n1*q2)<tol)
+        if (norm(A*x2-rm_n1*x2)<tol)
+            disp('tolerance met');
             break;
+        end
+        if iter == 90
+            disp(rp_n);
+            disp(rm_n);
         end
         rp_n = rp_n1;
         rm_n = rm_n1;
         x1 = q1;
         x2 = q2;
+        iter = iter + 1;
     end
 
 end
